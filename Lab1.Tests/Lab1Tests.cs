006a7de081355212f0ef4837bf8bcb40b972afca
Picadio <sashabedryk2004@gmail.com>
2024-09-25 21:32:08 +0300

    more tests

--- Lab1.Tests/Lab1Tests.cs
+++ Lab1.Tests/Lab1Tests.cs
@@ -31,4 +31,19 @@ public class Lab1Tests
         var answerStr = answer.Aggregate("", (current, i) => current + (i + " "));
         Assert.True(answerStr.Trim() == "1 3 2");
     }
+    [Fact]
+    public void TestValidating()
+    {
+        Assert.False(ValidateUtil.Validate(-1, -2));
+        Assert.False(ValidateUtil.Validate(-1, 0));
+        Assert.False(ValidateUtil.Validate(2, 10));
+        Assert.True(ValidateUtil.Validate(3, 4));
+        Assert.True(ValidateUtil.Validate(12, 100));
+    }
+    [Fact]
+    public void TestGetFilledList()
+    {
+        Assert.NotEmpty(MathUtil.GetFilledList(1));
+        Assert.Equal(MathUtil.GetFilledList(3), new []{1, 2, 3});
+    }
 }
